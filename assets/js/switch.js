c10d58594558fe94e85e6d609b5176685bfd63c4
arseniysij <50396694+arseniysij@users.noreply.github.com>
2025-06-12 08:31:53 +0500

    Update switch.js

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -1,6 +1,8 @@
-$('body').animate({opacity: 0}, 1000, function() {
+document.addEventListener('DOMContentLoaded', function () {
+  $('body').animate({opacity: 0}, 1000, function() {
     $(this).remove();
 	setTimeout(() => {
 		window.location.href = "https://youtu.be/9X_Ne3S5FAU";
 	}, 3000);
 });
+})
